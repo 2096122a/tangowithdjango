ff13b471b5206293e480a42443e7d89b1ad4137f
Kenneth Anderson <22096122A@STUDENT.GLA.AC.UK>
2015-03-06 17:14:59 +0000

    Chapter 15 completed

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -10,8 +10,23 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from datetime import datetime
+from rango.bing_search import run_query
 
 
+
+
+def search(request):
+
+    result_list = []
+
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+
+        if query:
+            # Run our Bing function to get the results list!
+            result_list = run_query(query)
+
+    return render(request, 'rango/search.html', {'result_list': result_list})
 
 def add_page(request, category_name_slug):
 
